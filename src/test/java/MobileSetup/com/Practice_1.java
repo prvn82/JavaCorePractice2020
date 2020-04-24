768d21d87b56091747d38a8ebab7e160a7c73e3b
Symanpraveen <46296839+Symanpraveen@users.noreply.github.com>
2020-04-25 00:42:14 +0530

    New Changes

--- src/test/java/MobileSetup/com/Practice_1.java
+++ src/test/java/MobileSetup/com/Practice_1.java
@@ -6,37 +6,32 @@ import java.util.List;
 
 public class Practice_1 {
 
-	@SuppressWarnings("unlikely-arg-type")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Write a java program to find the duplicate words and their number of occurrences in a string?
+		// Write a java program to find the duplicate words and their number of
+		// occurrences in a string?
 
-     
-		
 		String Str = "My MY Name is Praveen";
-		
+
 		List<String> check = new LinkedList<String>();
-		
-		for(int i=0;i<Str.length();i++) {
-			
-		
-			
-			if(check.contains(Character.toString(Str.charAt(i)))) {
+
+		for (int i = 0; i < Str.length(); i++) {
+
+			if (check.contains(Character.toString(Str.charAt(i)))) {
 				continue;
-			}
-			else {
-			int count =1;
-			for(int j =i+1;j<Str.length();j++) {
-				//System.out.println(Str.charAt(i) + " "+ Str.charAt(j));
-				if(Str.charAt(i)==Str.charAt(j)) {
-					count++;
+			} else {
+				int count = 1;
+				for (int j = i + 1; j < Str.length(); j++) {
+
+					if (Str.charAt(i) == Str.charAt(j)) {
+						count++;
+					}
 				}
-			}
-			
-			if(count>1) {
-				check.add(Character.toString(Str.charAt(i)));
-				System.out.println(Str.charAt(i)+ " Has total number of "+count+ " elements in string");
-			}
+
+				if (count > 1) {
+					check.add(Character.toString(Str.charAt(i)));
+					System.out.println(Str.charAt(i) + " Has total number of " + count + " elements in string");
+				}
 			}
 		}
 
